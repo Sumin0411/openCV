4e01b1e3f7ab0a46e44c576c41dd95a5aa300d2e
Sumin0411 <kimsumin411@gmail.com>
2024-01-22 17:19:24 +0900

    open cv ex

--- Desktop/opencv/chap05/04.arithmethic_op.py
+++ Desktop/opencv/chap05/04.arithmethic_op.py
@@ -3,14 +3,16 @@ import numpy as np, cv2
 m1 = np.full((3, 6), 10, np.uint8)			# 단일 채널 생성 및 초기화
 m2 = np.full((3, 6), 50, np.uint8)
 m_mask = np.zeros(m1.shape, np.uint8)		# 
-m_mask[         ] = 1							# 
+m_mask[:, 3: ] = 1							# 
 
-m_add1 = cv2.add( )                    # 
-m_add2 = cv2.add(         )       # 
+m_add1 = cv2.add(m1, m2)                    # 
+m_add2 = cv2.add(m1, m2, mask=m_mask)       # 
 
-# 
-
-
+#행렬나눗셈 수행
+m_div1 = cv2.divide(m1, m2)
+m1 = m1.astype(np.float32)
+m2 = m2.float32(m2)
+m_div2 = cv2.divide(m1, m2)
 
 
 titles = ['m1', 'm2', 'm_mask','m_add1','m_add2','m_div1', 'm_div2']
