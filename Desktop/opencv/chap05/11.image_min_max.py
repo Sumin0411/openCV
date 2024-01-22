1bbb29cf71b7bc3f860058347854854dea315df6
Sumin0411 <kimsumin411@gmail.com>
2024-01-22 17:41:06 +0900

    원소의 최솟값 및 최댓값 구하기

--- Desktop/opencv/chap05/11.image_min_max.py
+++ Desktop/opencv/chap05/11.image_min_max.py
@@ -1,12 +1,13 @@
 import numpy as np, cv2
 
-image = cv2.imread("Source/chap05/images/minMax.jpg", cv2.IMREAD_GRAYSCALE)
+image = cv2.imread("/Users/gimsumin/Desktop/opencv/chap05/images/minMax.jpg", cv2.IMREAD_GRAYSCALE)
 if image is None: raise Exception("영상 파일 읽기 오류 발생")
 
+min_val,max_val, _, _ = cv2.minMaxLoc(image)
 
-
-
-
+ratio = 255 / (max_val - min_val)
+dst = np.round((image - min_val) * ratio).astype('uint8')
+min_dst, max_dst, _, _ = cv2.minMaxLoc(dst)
 
 
 
